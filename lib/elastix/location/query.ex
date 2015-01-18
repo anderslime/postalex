fa4488a946abde14504adcf5ec94153389014480
Thomas Ringling <thomas.ringling@gmail.com>
2015-01-18 13:04:16 +0100

    Inject clients and extract client logic into client

--- lib/elastix/location/query.ex
+++ lib/elastix/location/query.ex
@@ -1,4 +1,45 @@
 defmodule Elastix.Location.Query do
+  alias Elastix.Client, as: ESClient
+
+  def locations(:postal_districts, ctry_cat, kinds, postal_districts) do
+    query = postal_district_query(kinds, postal_districts)
+    execute(query, index_type(ctry_cat))
+  end
+
+  def locations(:bounding_box, ctry_cat, kinds, bounding_box) do
+    query = bounding_box(kinds, bounding_box.bottom_left, bounding_box.top_right)
+    execute(query, index_type(ctry_cat))
+  end
+
+  defp execute(query, { index, type }) do
+    ESClient.execute(:search, query, index, type)
+      |> location_response
+  end
+
+  defp location_response(response) do
+    %{ total: total(response), locations: locations(response), es_time_ms: time(response) }
+  end
+
+  defp map_to_loc(res) do
+    res["_source"]
+      |> Map.delete("_id")
+      |> Map.delete("_rev")
+  end
+
+  defp index_type(%{ country: country, category: category }) do
+    { "#{country}_#{category}_locations", "location" }
+  end
+  defp total(response), do: response["hits"]["total"]
+  defp time(response), do: response["took"]
+  defp hits(response), do: response["hits"]["hits"]
+
+  defp locations(response) do
+    response |> hits |> Enum.map fn(res)-> map_to_loc(res) end
+  end
+
+  #######
+  # Query
+
   def postal_district_query(kinds, postal_districts, options \\ %{size: 1000}) do
     %{
       size: options.size,
--- lib/service/location.ex
+++ lib/service/location.ex
@@ -1,64 +1,58 @@
 defmodule Postalex.Service.Location do
 
   alias Postalex.Service.Area
+  alias Postalex.Service.PostalCode
 
   def find(ctry_cat, kinds, postal_districts) do
-    _find(ctry_cat, kinds, postal_districts)
+    find(ctry_cat, kinds, postal_districts, default_clients)
+  end
+  def find(ctry_cat, kinds, postal_districts, clients) do
+    clients.location_query.locations(:postal_districts, ctry_cat, kinds, postal_districts)
   end
 
   def find_by_area_slug(ctry_cat, kinds, area_slug) do
-    area = Area.by_slug(ctry_cat, area_slug)
-    _find(ctry_cat, kinds, postal_district_ids(area_slug, area))
+    find_by_area_slug(ctry_cat, kinds, area_slug, default_clients)
+  end
+  def find_by_area_slug(ctry_cat, kinds, area_slug, clients) do
+    area = Area.by_slug(ctry_cat, area_slug, clients)
+    postal_districts = postal_district_ids(area_slug, area)
+    clients.location_query.locations(:postal_districts, ctry_cat, kinds, postal_districts)
+  end
+
+  def by_bounding_box(ctry_cat, kinds, bounding_box) do
+    by_bounding_box(ctry_cat, kinds, bounding_box, default_clients)
+  end
+  def by_bounding_box(ctry_cat, kinds, bounding_box, clients) do
+    clients.location_query.locations(:bounding_box, ctry_cat, kinds, bounding_box)
+  end
+
+  def district_locations_by_postal_code(ctry_cat, kinds, postal_code) do
+    district_locations_by_postal_code(ctry_cat, kinds, postal_code, default_clients)
+  end
+  def district_locations_by_postal_code(ctry_cat, kinds, postal_code, clients) do
+    pd_id = PostalCode.postal_district_id(ctry_cat, postal_code, clients)
+    postal_districts_locations(ctry_cat, kinds, pd_id, clients)
   end
 
   defp postal_district_ids(area_slug, nil) do
     raise "Area with slug '#{area_slug}' not found"
   end
+
   defp postal_district_ids(_, area) do
     area.postal_districts |> Enum.map fn(pd)-> pd.id end
   end
 
-  def by_bounding_box(ctry_cat, kinds, bounding_box) do
-    { index, type } = index_type(ctry_cat)
-    query = Elastix.Location.Query.bounding_box(kinds, bounding_box.bottom_left, bounding_box.top_right)
-    Elastix.Client.execute(:search, query, index, type)
-    |> location_response
+  defp postal_districts_locations(ctry_cat, kinds, nil, _), do: %{ total: 0, locations: [], es_time_ms: -1 }
+  defp postal_districts_locations(ctry_cat, kinds, postal_district, clients) do
+    clients.location_query.locations(:postal_districts, ctry_cat, kinds, [postal_district])
   end
 
-  def district_locations_by_postal_code(ctry_cat, kinds, postal_code) do
-    pd_id = Postalex.Service.PostalCode.postal_district_id(ctry_cat, postal_code)
-    postal_districts_locations(ctry_cat, kinds, pd_id)
-  end
-
-  defp postal_districts_locations(ctry_cat, kinds, nil), do: %{ total: 0, locations: [], es_time_ms: -1 }
-  defp postal_districts_locations(ctry_cat, kinds, postal_district) do
-    _find(ctry_cat, kinds, [postal_district])
-  end
-
-  defp _find(ctry_cat, kinds, postal_districts) do
-    { index, type } = index_type(ctry_cat)
-    query = Elastix.Location.Query.postal_district_query(kinds, postal_districts)
-    Elastix.Client.execute(:search, query, index, type)
-    |> location_response
-  end
-
-  defp location_response(response) do
-    %{ total: total(response), locations: locations(response), es_time_ms: time(response) }
-  end
-
-  defp map_to_loc(res) do
-    res["_source"] |> Map.delete("_id") |> Map.delete("_rev")
-  end
-
-  defp index_type(%{ country: country, category: category }) do
-    { "#{country}_#{category}_locations", "location" }
-  end
-  defp total(response), do: response["hits"]["total"]
-  defp time(response), do: response["took"]
-  defp hits(response), do: response["hits"]["hits"]
-
-  defp locations(response) do
-    response |> hits |> Enum.map fn(res)-> map_to_loc(res) end
+  defp default_clients do
+    %{
+      location_query: Elastix.Location.Query,
+      couch_client: CouchClient,
+      location_aggregation: Elastix.Location.Aggregation
+    }
   end
 
 end
