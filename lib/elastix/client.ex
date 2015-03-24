b528f34ffe934af0d4646bf4628bb4892b95f9c5
Mads Ohm Larsen <mads.ohm@gmail.com>
2015-03-24 16:06:42 +0100

    Let elastic url be a class variable

--- lib/elastix/client.ex
+++ lib/elastix/client.ex
@@ -1,8 +1,9 @@
 defmodule Elastix.Client do
+  @elastic_url System.get_env["ELASTIC_SERVER_URL"]
 
   def ping do
     try do
-      HTTPotion.get(elastic_url)
+      HTTPotion.get(@elastic_url)
       {:ok, "elastic"}
     rescue
       e in HTTPotion.HTTPError -> {:error, "elastic: #{e.message}"}
@@ -10,12 +11,11 @@ defmodule Elastix.Client do
   end
 
   def execute(:search, query, index, type) do
-    "#{elastic_url}/#{index}/#{type}/_search"
+    "#{@elastic_url}/#{index}/#{type}/_search"
       |> post(query)
       |> body
       |> Poison.decode!
   end
-
   def execute(_, _, _, _), do: {:error, :undef_query_type}
 
   defp post(url, query) do
@@ -23,7 +23,4 @@ defmodule Elastix.Client do
   end
 
   defp body(res), do: res.body
-
-  defp elastic_url, do: System.get_env["ELASTIC_SERVER_URL"]
-
 end
