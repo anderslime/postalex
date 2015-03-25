d5876eac8febc8b590b9e8cafb54b460807f4249
Anders Emil Nielsen <aemilnielsen@gmail.com>
2015-03-25 10:43:32 +0100

    Fix elastic url config reader

--- lib/elastix/client.ex
+++ lib/elastix/client.ex
@@ -1,9 +1,7 @@
 defmodule Elastix.Client do
-  @elastic_url Application.get_env(:postalex, :elastic_server_url)
-
   def ping do
     try do
-      HTTPotion.get(@elastic_url)
+      HTTPotion.get(elastic_url)
       {:ok, "elastic"}
     rescue
       e in HTTPotion.HTTPError -> {:error, "elastic: #{e.message}"}
@@ -11,7 +9,7 @@ defmodule Elastix.Client do
   end
 
   def execute(:search, query, index, type) do
-    "#{@elastic_url}/#{index}/#{type}/_search"
+    "#{elastic_url}/#{index}/#{type}/_search"
       |> post(query)
       |> body
       |> Poison.decode!
@@ -19,7 +17,7 @@ defmodule Elastix.Client do
   def execute(_, _, _, _), do: {:error, :undef_query_type}
 
   def mapping(index, type) do
-    "#{@elastic_url}/#{index}/_mapping/#{type}"
+    "#{elastic_url}/#{index}/_mapping/#{type}"
     |> HTTPotion.get
     |> body
     |> Poison.decode!
@@ -30,4 +28,6 @@ defmodule Elastix.Client do
   end
 
   defp body(res), do: res.body
+
+  defp elastic_url, do: Application.get_env(:postalex, :elastic_server_url)
 end
