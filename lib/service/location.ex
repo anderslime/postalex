5d24a8708c61f5404f3726674c2cfa3a2914f7c5
Mads Ohm Larsen <mads.ohm@gmail.com>
2015-03-26 11:08:52 +0100

    Revert "Merge pull request #12 from omegahm/add-sorting"
    This reverts commit ab2c37c6a8d626c735c0e8195d92b176d19575fb, reversing
    changes made to 19a9b699e563de5eae6e658dcc7baf94fbc34efd.

--- lib/service/location.ex
+++ lib/service/location.ex
@@ -3,35 +3,35 @@ defmodule Postalex.Service.Location do
   alias Postalex.Service.Area
   alias Postalex.Service.PostalCode
 
-  def find(ctry_cat, kinds, postal_districts, sort) do
-    find(ctry_cat, kinds, postal_districts, sort, default_clients)
+  def find(ctry_cat, kinds, postal_districts) do
+    find(ctry_cat, kinds, postal_districts, default_clients)
   end
-  def find(ctry_cat, kinds, postal_districts, sort, clients) do
-    clients.location_query.locations(:postal_districts, ctry_cat, kinds, postal_districts, sort)
+  def find(ctry_cat, kinds, postal_districts, clients) do
+    clients.location_query.locations(:postal_districts, ctry_cat, kinds, postal_districts)
   end
 
-  def find_by_area_slug(ctry_cat, kinds, area_slug, sort) do
-    find_by_area_slug(ctry_cat, kinds, area_slug, sort, default_clients)
+  def find_by_area_slug(ctry_cat, kinds, area_slug) do
+    find_by_area_slug(ctry_cat, kinds, area_slug, default_clients)
   end
-  def find_by_area_slug(ctry_cat, kinds, area_slug, sort, clients) do
+  def find_by_area_slug(ctry_cat, kinds, area_slug, clients) do
     area = Area.by_slug(ctry_cat, area_slug, clients)
     postal_districts = postal_district_ids(area_slug, area)
-    clients.location_query.locations(:postal_districts, ctry_cat, kinds, postal_districts, sort)
+    clients.location_query.locations(:postal_districts, ctry_cat, kinds, postal_districts)
   end
 
-  def by_bounding_box(ctry_cat, kinds, bounding_box, sort) do
-    by_bounding_box(ctry_cat, kinds, bounding_box, sort, default_clients)
+  def by_bounding_box(ctry_cat, kinds, bounding_box) do
+    by_bounding_box(ctry_cat, kinds, bounding_box, default_clients)
   end
-  def by_bounding_box(ctry_cat, kinds, bounding_box, sort, clients) do
-    clients.location_query.locations(:bounding_box, ctry_cat, kinds, sort, bounding_box)
+  def by_bounding_box(ctry_cat, kinds, bounding_box, clients) do
+    clients.location_query.locations(:bounding_box, ctry_cat, kinds, bounding_box)
   end
 
-  def district_locations_by_postal_code(ctry_cat, kinds, postal_code, sort) do
-    district_locations_by_postal_code(ctry_cat, kinds, postal_code, sort, default_clients)
+  def district_locations_by_postal_code(ctry_cat, kinds, postal_code) do
+    district_locations_by_postal_code(ctry_cat, kinds, postal_code, default_clients)
   end
-  def district_locations_by_postal_code(ctry_cat, kinds, postal_code, sort, clients) do
+  def district_locations_by_postal_code(ctry_cat, kinds, postal_code, clients) do
     pd_id = clients.postal_code_client.postal_district_id(ctry_cat, postal_code, clients)
-    postal_districts_locations(ctry_cat, kinds, pd_id, sort, clients)
+    postal_districts_locations(ctry_cat, kinds, pd_id, clients)
   end
 
   defp postal_district_ids(area_slug, nil) do
@@ -42,9 +42,9 @@ defmodule Postalex.Service.Location do
     area.postal_districts |> Enum.map fn(pd)-> pd.id end
   end
 
-  defp postal_districts_locations(_, _, nil, _, _), do: %{ total: 0, locations: [], es_time_ms: -1 }
-  defp postal_districts_locations(ctry_cat, kinds, postal_district, sort, clients) do
-    clients.location_query.locations(:postal_districts, ctry_cat, kinds, [postal_district], sort)
+  defp postal_districts_locations(ctry_cat, kinds, nil, _), do: %{ total: 0, locations: [], es_time_ms: -1 }
+  defp postal_districts_locations(ctry_cat, kinds, postal_district, clients) do
+    clients.location_query.locations(:postal_districts, ctry_cat, kinds, [postal_district])
   end
 
   defp default_clients do
